a6fed9c08997eb826fec97c0c04c4850b54c97db
9syncopation <9syncopation@gmail.com>
2020-03-18 05:38:46 +0200

    css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,34 +7,43 @@ export default function Header() {
   return (
     <header className="header">
       <img src={logo} alt="LOGO" className="logo" />
-      <nav >
-        <ul >
-          <div>
-            <li>
-              <Link to="/"> Home</Link>
-            </li>
+      <nav>
+        <ul>
+          <div className="main-menu">
+            <div>
+              <li>
+                <Link to="/"> Home</Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/About"> About</Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/PetsPage"> All Pets</Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/UserPets"> My Pets</Link>
+              </li>
+            </div>
+            <div className="contact-details">
+              <div>
+                <li>
+                  <Link to="/ContactPage"> Contact</Link>
+                </li>
+              </div>
+              <div>
+                <li>
+                  <Link to="/Login"> Login</Link>
+                </li>
+              </div>
+            </div>
           </div>
-          <div>
-            <li>
-              <Link to="/About"> About</Link>
-            </li>
-          </div>
-          <div>
-          <li>
-          <Link to="/PetsPage"> All Pets</Link>
-          </li>
-          </div>
-          <div>
-          <li>
-          <Link to="/UserPets"> My Pets</Link>
-          </li>
-          </div>
-          <div>
-            <li>
-              <Link to="/ContactPage"> Contact</Link>
-            </li>
-          </div>
-          </ul>
+        </ul>
       </nav>
     </header>
   );
